176efc61385cd80bc1c35470bcda4492ce5273ef
root <root@www.opendoorcoworking.com>
2015-10-17 01:42:25 -0400

    more email fixes

--- contact.php
+++ contact.php
@@ -3,15 +3,15 @@ $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
 $field_message = $_POST['cf_message'];
 
-$mail_to = 'my@email.com';
+$mail_to = 'jflynn8@gmail.com';
 $subject = 'Message from a site visitor '.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: contact@opendoorcoworking.com \n';
-$body_message .= 'Message: '.$field_message . 'From: ' . $field_email;
+$body_message .= 'E-mail:' .  $field_email . " \n";
+$body_message .= 'Message: '.$field_message;
 
-$headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
+$headers = "From: contact@opendoorcoworking.com \n";
+$headers .= "Reply-To: contact@opendoorcoworking.com \n";
 
 $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
