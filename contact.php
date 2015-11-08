3edca722a3d6441184f686701a6505a8402e243b
J.D. Flynn <james.flynn@milespartnership.com>
2015-11-07 20:08:15 -0600

    tweaking contact

--- contact.php
+++ contact.php
@@ -4,10 +4,10 @@ $field_email = $_POST['cf_email'];
 $field_message = $_POST['cf_message'];
 
 $mail_to = 'jd@opendoorcoworking.com';
-$subject = 'Message from a site visitor\: '.$field_name;
+$subject = 'Message from a site visitor: '.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail:' .  $field_email . " \n";
+$body_message .= 'E-mail: ' .  $field_email . " \n";
 $body_message .= 'Message: '.$field_message;
 
 $headers = "From: contact@opendoorcoworking.com \n";
