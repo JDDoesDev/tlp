8edd9d74ea254cd112747fcda0d101dca2a98b42
J.D. Flynn <james.flynn@milespartnership.com>
2015-11-12 09:44:39 -0600

    contact form split testing

--- contact.php
+++ contact.php
@@ -1,14 +1,14 @@
 <?php
 $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
-$field_message = $_POST['cf_message'];
+$field_message = $_POST['cf_message'];$version = $_POST['page-version'];
 
 $mail_to = 'jd@opendoorcoworking.com';
 $subject = 'Message from a site visitor: '.$field_name;
 
-$body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: ' .  $field_email . " \n";
-$body_message .= 'Message: '.$field_message;
+$body_message = 'From: '. $field_name."\n";
+$body_message .= 'E-mail: ' . $field_email . " \n";
+$body_message .= 'Message: '. $field_message . " \n";$body_message .= 'Version: '. $version;
 
 $headers = "From: contact@opendoorcoworking.com \n";
 $headers .= "Reply-To: contact@opendoorcoworking.com \n";
