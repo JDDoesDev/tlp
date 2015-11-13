782c23e86c65dc25513d8a8be8c7de1554d39fdf
jddoesdev <jd@jamesdflynn.com>
2015-11-12 20:24:52 -0600

    cookie, not session

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
-session_start();
-$pages = array(1,2,3);
-shuffle($pages);
-$page = $pages[0];
-
+if (isset($_COOKIE['opendoor_page_version'])) {
+  $page = $_COOKIE['opendoor_page_version'];
+} else {
+  $pages = array(1,2,3);
+  shuffle($pages);
+  $page = $pages[0];
+  $expire = time() + (60 * 60 * 24 * 3);
+  setcookie('opendoor_page_version', $page, $expire);
+}
 switch ($page) {
   case 1:
     include_once('aindex.html');
