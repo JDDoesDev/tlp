81e58b744b9d641c66d1e9898a057d0d29925642
jddoesdev <jd@jamesdflynn.com>
2016-04-02 21:21:44 -0500

    changing some copy

--- index.php
+++ index.php
@@ -108,7 +108,7 @@
           "http://www.yelp.com/biz/open-door-coworking-la-porte"
           ],
         "logo": "http://www.opendoorcoworking.com/images/opendoorcowork.png",
-        "description": "Open Door Coworking is La Porte, Indiana's first coworking space for freelancers, entrepreneurs, and telecommuters. Ready to join the coworking movement?  Work. Together.",
+        "description": "Open Door Coworking is La Porte, Indiana's first coworking space for freelancers, entrepreneurs, and telecommuters. A coworking space is an alternative to traditional office space with a focus on creating a collaborative work community. Work. Together.",
         "contactPoint": {
           "@type": "ContactPoint",
           "telephone": "+12195610219",
@@ -239,8 +239,10 @@
                   the use of an office or other working environment by people who are self-employed or working for different employers, typically so as to share equipment, ideas, and knowledge.</br>
                 </p>
                 <p>
-                  The whole idea of coworking is to bring bright, creative people together and let the ideas collide
-                  <!-- Be more creative, grow your business faster, and have a better work-life balance. -->
+                  The whole idea of coworking is to bring bright, creative people together and let the ideas collide in an altenative environment to traditional office space.
+                </p>
+                <p>
+                  Be more creative, grow your business faster, and have a better work-life balance.
                 </p>
                 <a class="button animated fadeInUp delay-1s first" href="index.html#learn" onclick="ga('send', 'event', 'button', 'Click', 'Learn');"><span><i class="cogelegance-icons-"></i></span>Learn More</a><a class="button border animated fadeInUp delay-1s" href="http://goo.gl/forms/ook9usnqsn" target="_blank" onclick="ga('send', 'event', 'Button', 'Click', 'tour');">Schedule a Tour</a>
               </div>
@@ -273,24 +275,25 @@
                 </p>
               </div>
             </div>
+            <div class="w-col w-col-4   delay-1s">
+              <div class="icon-box">
+                <i class="fa fa-lightbulb-o"></i>
+                <h4 class="icons-heading">INDEPENDENTLY</h4>
+                <p>
+                  When you're at a coworking space, you're in an evironment that's designed to let you do the best work you can and innovate with minimal distractions.  You get the best of both worlds:  Professional office space and autonomy to work how you want when you want.
+                </p>
+              </div>
+            </div>
             <div class="w-col w-col-4   delay-05s">
               <div class="icon-box">
                 <i class="fa fa-users"></i>
-                <h4 class="icons-heading">COLLABORATE</h4>
+                <h4 class="icons-heading">TOGETHER</h4>
                 <p>
                   The coworking community is all about being independent together. Collaboration is easy when you're surrounded by great people.
                 </p>
               </div>
             </div>
-            <div class="w-col w-col-4   delay-1s">
-              <div class="icon-box">
-                <i class="fa fa-lightbulb-o"></i>
-                <h4 class="icons-heading">INNOVATE</h4>
-                <p>
-                  When you're at a coworking space, you're in an evironment that's designed to let you do the best work you can and innovate with minimal distractions.
-                </p>
-              </div>
-            </div>
+            <div class="clearfix"></div>
             <h2>Still curious about what a coworking space is?</h2>
             <a href="#contact" class="button" onclick="ga('send', 'event', 'button', 'Click', 'learn contact');">Ask us</a>
           </div>
@@ -309,7 +312,7 @@
           <div class="center">
             <h1 class="underline"> Look Inside Open Door Coworking </h1>
             <a href="images/slider/cowork1.jpg" data-lightbox="opendoor"><img src="images/slider/cowork1.jpg" height="25%" width="25%" alt="a desk"></a>
-            <a href="images/slider/cowork2.jpg" data-lightbox="opendoor"><img src="images/slider/cowork2.jpg" height="25%" width="25%" alt="a desk"></a>
+            <a href="images/slider/cowork2.jpg" data-lightbox="opendoor"><img src="images/slider/cowork2.jpg" height="25%" width="25%" alt="another desk"></a>
             <a href="images/slider/cowork3.jpg" data-lightbox="opendoor"><img src="images/slider/cowork3.jpg" height="25%" width="25%" alt="conference room"></a>
             <a href="images/slider/cowork4.jpg" data-lightbox="opendoor"><img src="images/slider/cowork4.jpg" height="25%" width="25%" alt="main entrance"></a>
             <a href="images/slider/cowork5.jpg" data-lightbox="opendoor"><img src="images/slider/cowork5.jpg" height="25%" width="25%" alt="private office"></a>
@@ -353,13 +356,13 @@
               Networking events
             </li>
             <li>
-              Location in La Porte's Urban Enterprise Zone
+              Location in <a href="http://www.gledc.com/laporte-urban-enterprise-zone.html" target="_blank">La Porte's Urban Enterprise Zone</a>
             </li>
             <li>
               Business listed on Members' Wall
             </li>
             <li>
-              No minimum membership term
+              No minimum membership term or leases
             </li>
           </ul>
         </div>
