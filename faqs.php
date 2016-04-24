1340fc0b27d0520dce675ff82ad4564e48ef736d
jddoesdev <jd@jamesdflynn.com>
2016-04-23 20:02:27 -0500

    faqs

---
+++ faqs.php
@@ -0,0 +1,73 @@
+
+<?php include 'includes/header.inc'; ?>
+    <div class="header-parallax not-front" data-stellar-background-ratio="0.5">
+      <div class="slidersection">
+        <div class="overlay">
+          <div class="center fixed-content">
+            <div class="center-fix">
+              <h1 class="underline animated fadeInDown">YOU <strong>GOT</strong> QUESTIONS?</h1>
+              <h4 class="animated fadeInUp delay-05s" style="margin-bottom: 10px;">We've got <strong class="green">anwsers.</strong></h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--///////////////////////////////////////////////////////
+
+    // End slider section
+
+    //////////////////////////////////////////////////////////-->
+
+    <div id="faqs" class='center'>
+      <div class="row-back">
+        <div class="w-container wrap">
+          <h2>Frequently Asked Questions</h2>
+          <ul class="questions left">
+            <li>
+              <div class="question">
+                What is a coworking space?
+              </div>
+              <div class="answer">
+                A coworking space is, at its core, shared office space, but it goes much deeper than that.  It's an alternative to traditional office space for people who otherwise wouldn't have access to office space.  One thing that separates a coworking space from other office  options is the community that comes with a coworking space.  It's a community of other like-minded professionals who become your coworkers and help expand your network.
+              </div>
+            </li>
+            <li>
+              <div class="question">
+                What is coworking?
+              </div>
+              <div class="answer">
+                Coworking is a style of working for freelancers, entrepreneurs, small-business owners, telecommuters, or anyone else who needs a place to work to gather, share ideas, and build a community.  A key benefit of coworking compared to working from home or a coffee shop is that you get to work around people and you have the advantage of being able to actually talk to them.
+              </div>
+            </li>
+            <li>
+              <div class="question">
+                Why should I join a coworking space instead of renting office space?
+              </div>
+              <div class="answer">
+                If you're ready for office space, then go for it!  I don't want to hold anyone back!  If you're just starting out and not ready to sign a lease; you're growing, but not ready for the costs of renting a storefront or office space; or you're working remotely and just need a place to work that isn't your home or a coffee shop then a coworking space is perfect for you.  Many office spaces require leases of one, two, or more years and don't include anything except the building.  Coworking spaces usually include internet, all utilities, and flexible <a href="/membership/">membership options</a>.
+              </div>
+            </li>
+            <li>
+              <div class="question">
+                Is a coworking space the same as an incubator, renting an office, or something else?
+              </div>
+              <div class="answer">
+                Coworking shares similarities with incubators and accelerators, but is its own entity.  Coworking is more focused on building a community while incubators are more focused on getting a budding business started.  Often times an incubator will allow you to work for free in return for a percentage of the company being incubated.  Coworking spaces are not focused on office rental, but often times a coworking space will include a few private offices for members who choose a higher tier membership.
+              </div>
+            </li>
+            <li>
+              <div class="question">
+                Why did you open a coworking space?
+              </div>
+              <div class="answer">
+                Coworking is a relatively new concept.  The first "coworking space" was opened in 2005 and they have popped up all over the place, especially prevalent in larger cities or in cities with a strong tech community.  When I got a job as a remote web developer, I found that I wasn't working as well as I could be and I needed a solution to keep me on track.  I tried a few coffee shops which were great, but it's easy to get distracted by conversations that you might overhear and become interested in.  I then did some research on coworking spaces and found that there was one in the counties east and west of La Porte County, but nothing like it in La Porte, Michigan City, or anywhere else in the county, so I decided to open one myself!
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include 'includes/footer.inc'; ?>
+<?php include 'includes/json_ld.inc'; ?>
